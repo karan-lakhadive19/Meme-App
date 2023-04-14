15b1a54f37b1df6634329ecf88f2bc10d8a2d808
Karan Lakhadive <lakhadive1926@gmail.com>
2023-04-14 15:52:38 +0530

    initial commit

---
+++ lib/screens/meme.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Meme extends StatefulWidget {
+  const Meme({super.key});
+
+  @override
+  State<Meme> createState() => _MemeState();
+}
+
+class _MemeState extends State<Meme> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+        title: Text(
+          'Explore Memes',
+          style: GoogleFonts.roboto(fontWeight: FontWeight.bold),
+        ),
+        toolbarHeight: 80,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(30),
+          ),
+        ),
+      ),
+    );
+  }
+}
