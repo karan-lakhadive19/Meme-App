7b106629ee0c972789e75f89d7180e0d65bcdb07
Karan Lakhadive <lakhadive1926@gmail.com>
2023-04-15 13:46:08 +0530

    Next Button Functionality

--- lib/screens/meme.dart
+++ lib/screens/meme.dart
@@ -1,8 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart' as http;
+import 'package:meme/main.dart';
 
 class Meme extends StatefulWidget {
   const Meme({super.key});
@@ -11,21 +14,18 @@ class Meme extends StatefulWidget {
   State<Meme> createState() => _MemeState();
 }
 
-String stringResponse='';
-Map mapResponse={};
-Map dataResponse={};
-
+String stringResponse = '';
+Map mapResponse = {};
+Map dataResponse = {};
 
 class _MemeState extends State<Meme> {
-
   Future apicall() async {
     http.Response response;
     response = await http.get(Uri.parse("https://meme-api.com/gimme"));
-    if(response.statusCode==200) {
+    if (response.statusCode == 200) {
       setState(() {
         stringResponse = response.body;
         mapResponse = json.decode(response.body);
-        
       });
     }
   }
@@ -37,36 +37,100 @@ class _MemeState extends State<Meme> {
     super.initState();
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 235, 192, 177),
-      appBar: AppBar(
-        backgroundColor: Colors.green,
-        centerTitle: true,
-        title: Text(
-          'Explore Memes',
-          style: GoogleFonts.roboto(fontWeight: FontWeight.bold),
-        ),
-        toolbarHeight: 80,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.vertical(
-            bottom: Radius.circular(30),
+        backgroundColor: Color.fromARGB(255, 235, 192, 177),
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+          centerTitle: true,
+          title: Text(
+            'Memes',
+            style: GoogleFonts.roboto(fontWeight: FontWeight.bold),
+          ),
+          toolbarHeight: 80,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(
+              bottom: Radius.circular(30),
+            ),
           ),
         ),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            Container(
-              height: MediaQuery.of(context).size.height*.75,
-              width: MediaQuery.of(context).size.width,
-              child: mapResponse['url']==null? Center(child: CircularProgressIndicator()):Image.network(mapResponse['url']),
-            )
-          ],
-        ),
-      )
-    );
+        body: Center(
+          child: Column(
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height * .80,
+                width: MediaQuery.of(context).size.width,
+                child: mapResponse['url'] == null
+                    ? Center(child: CircularProgressIndicator())
+                    : Image.network(mapResponse['url']),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width*.4,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Handle button press
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.green, // Background color of button
+                        elevation: 3, // Depth of the button
+                        shape: RoundedRectangleBorder(
+                          
+                          borderRadius:
+                              BorderRadius.circular(20), // Rounded corners
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 10, horizontal: 20),
+                        child: Text(
+                          'Share',
+                          style: GoogleFonts.roboto(
+                            fontWeight: FontWeight.bold
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    
+                    width: MediaQuery.of(context).size.width*.4,
+                    child: ElevatedButton(
+                    
+                      onPressed: () {
+                        apicall();
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.green, // Background color of button
+                        elevation: 3, // Depth of the button
+                        shape: RoundedRectangleBorder(
+                          borderRadius:
+                              BorderRadius.circular(20), // Rounded corners
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 10, horizontal: 20),
+                        child: Text(
+                          'Next',
+                          style: GoogleFonts.roboto(
+                            fontWeight: FontWeight.bold
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  
+                ],
+              )
+            ],
+          ),
+        ));
   }
 }
