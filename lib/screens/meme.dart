a28d531e66d36447e0fd93b5ad9ae85a3e45f198
Karan Lakhadive <lakhadive1926@gmail.com>
2023-04-15 09:50:22 +0530

    Fetching the memes

--- lib/screens/meme.dart
+++ lib/screens/meme.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:http/http.dart' as http;
 
 class Meme extends StatefulWidget {
   const Meme({super.key});
@@ -8,12 +11,39 @@ class Meme extends StatefulWidget {
   State<Meme> createState() => _MemeState();
 }
 
+String stringResponse='';
+Map mapResponse={};
+Map dataResponse={};
+
+
 class _MemeState extends State<Meme> {
+
+  Future apicall() async {
+    http.Response response;
+    response = await http.get(Uri.parse("https://meme-api.com/gimme"));
+    if(response.statusCode==200) {
+      setState(() {
+        stringResponse = response.body;
+        mapResponse = json.decode(response.body);
+        
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    apicall();
+    super.initState();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromARGB(255, 235, 192, 177),
       appBar: AppBar(
-        backgroundColor: Colors.amber,
+        backgroundColor: Colors.green,
         centerTitle: true,
         title: Text(
           'Explore Memes',
@@ -26,9 +56,17 @@ class _MemeState extends State<Meme> {
           ),
         ),
       ),
-      body: Container(
-        child: Text('Hello world'),
-      ),
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height*.75,
+              width: MediaQuery.of(context).size.width,
+              child: mapResponse['url']==null? Center(child: CircularProgressIndicator()):Image.network(mapResponse['url']),
+            )
+          ],
+        ),
+      )
     );
   }
 }
